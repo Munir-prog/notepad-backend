6f2e2aeb5ef53f74a306292b7e2452b43a0803db
Munir Zikrullaev <munirprog@gmail.com>
2022-11-10 23:34:40 +0300

    add entities

---
+++ src/main/java/com/mprog/database/model/User.java
@@ -0,0 +1,49 @@
+package com.mprog.database.model;
+
+import com.mprog.database.model.role.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    private String lastname;
+
+    private String firstname;
+
+    private LocalDate birthday;
+
+    @Column(unique = true)
+    private String phone;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "users_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+}
---
+++ src/main/java/com/mprog/database/model/role/Role.java
@@ -0,0 +1,28 @@
+package com.mprog.database.model.role;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole role;
+
+}
